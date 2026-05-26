62d7e05059d8a834c98e64b848f79a9bfa2c6b25
Prenay123 <prenaysaidiska@gmail.com>
2026-05-26 11:53:00 +0530

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  "mongodb://mongodb-service:27017/fluiddb";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Mongo connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Fluid AI DevOps Challenge"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "healthy"
+  });
+});
+
+app.get("/orders", (req, res) => {
+  res.json([
+    { id: 101, item: "Laptop" },
+    { id: 102, item: "Phone" }
+  ]);
+});
+
+app.listen(3001, () =>
+  console.log("Order service running on port 3001")
+);
